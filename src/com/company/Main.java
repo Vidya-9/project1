f2fcb10663900d5b40e459889822f0fb85026d45
Vidya-9 <vidyagolta509@gmail.com>
2020-12-15 08:17:45 +0530

    committing form intelij

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int a,b;
+	Scanner in= new Scanner(System.in);
+	System.out.println("Enter two integers");
+	a=in.nextInt();
+	b=in.nextInt();
+	System.out.println(a+b);
+    }
+}
